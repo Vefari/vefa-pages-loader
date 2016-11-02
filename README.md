a6295fb5ee682362be4cdc532c3dcb966a934e04
Cobey Potter <cobey.potter@wellfireinteractive.com>
2016-11-02 15:33:21 -0400

    Add ability to process Markdown files, split into content sections, add slug (WILL CHANGE), and attach a template file.
    Update semvar

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # vefa-pages-loader
 Static site page development powered by Webpack
+
+## Special Markdown Attributes
+
+### %% Content Sections %%
+
+Using `%% variable_name %%` on its own line will apply the following general markdown content to a variable of the same name. In the following example, `page.content` would equal the first paragraph, `page.content_alt` would equal the second paragraph, and `page.content_alt_2` would equal the unordered list.
+
+```
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua.
+
+%% content_alt %%
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+%% content_alt_2 %%
+
+- Lorem ipsum dolor sit amet, 
+- consectetur adipisicing elit, 
+- sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+```
+
+The key value must be open with `%%` and closed with `%%` for proper processing. Initial content (such as the first paragraph above) will always be applied to `page.content`. You may override this assignment by starting your markdown with a defined content section key (`%% whatever %%`).
--- index.js
+++ index.js
@@ -1,1 +1,72 @@
+const path = require('path');
+const fs = require('fs');
+const utils = require('loader-utils');
+const yaml = require('js-yaml');
 
+const matter = require('gray-matter');
+const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true
+});
+
+module.exports = function(source){
+    this.cacheable && this.cacheable();
+    
+    // lets load the shared locals object
+    let locals = this.options.locals; 
+
+    // get which config options to load
+    let loader = utils.getLoaderConfig(this, "pages");
+    
+    let pug = utils.getLoaderConfig(this, "pug");
+
+    let page = matter(source);
+    
+    // compile content through markdown
+    // divide into individual page section key/values if needed
+    if (page.content) {
+        // trim based on Content Sections `%% key %%` (see README)
+        let content = page.content.trim().split("%%");
+        
+        if (content[0].length == 0){
+            content.shift();
+        };
+
+        if (content.length == 1) {
+            page.content = content[0];
+        } 
+        else {
+            let i = 0;
+            do {
+                // this is a variable assignment
+                if (content[i].trim().indexOf(" ") == -1){
+                    page.data[content[i].trim()] = locals.markdown( content[i + 1] );
+                    i++;
+                    i++;
+                } 
+                else {
+                    page["content"] = content[i];
+                    i++;
+                }                
+            } while (i < content.length);
+        }
+
+        page.content = locals.markdown( page.content );
+    }
+
+    // set up the appropriate filename
+    if (page.data.slug) {
+        source.resourcePath = `${page.data.slug}`;
+    }
+
+    // get the associated template
+    if (!page.data.template) {
+        page.data.template = loader.default_template;
+    }
+    page.data.template_url = `${loader.template_dir}/${page.data.template}.pug`
+    this.addDependency( path.resolve( page.data.template_url ) );
+    page.data.template = fs.readFileSync(page.data.template_url, 'utf8')
+
+    return page;
+}
