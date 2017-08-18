16bebdf4e1ca61f87bbaa253ad7105457ac4b299
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-08-18 16:22:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 'use strict'
-const path = require('path');
-const fs = require('fs');
-const utils = require('loader-utils');
-const yaml = require('js-yaml');
+const path = require('path')
+const fs = require('fs')
+const utils = require('loader-utils')
+const yaml = require('js-yaml')
 
-const matter = require('gray-matter');
-const md = require('markdown-it')({
-    html: true,
-    linkify: true,
-    typographer: true
-});
+const matter = require('gray-matter')
 
-module.exports = function(source){
-    this.cacheable && this.cacheable();
+module.exports = function (source) {
+    this.cacheable && this.cacheable(true)
     
     // get which config options to load
-    let loader = utils.getOptions(this);
+    let loader = utils.getOptions(this)
 
     // lets load the shared locals object
-    let locals = loader.locals; 
+    let locals = loader.locals 
 
-    let page = matter(source);
-    
+    let page = matter(source)
+
     // compile content through markdown
     // divide into individual page section key/values if needed
     if (page.content) {
@@ -29,22 +24,15 @@ module.exports = function(source){
         // trim based on Content Sections `%% key %%` (see README)
         let content = page.content.trim().split("%%");
         
-        if (content[0].length == 0){
-            content.shift();
-        };
+        if (content[0].length == 0) {
+            content.shift()
+        }
 
         if (content.length == 1) {
-            page.content = content[0];
+            page.content = content[0]
         } 
         else {
-            content.forEach(
-                (element) => {
-                    let temp = matteryaml.safeLoad(element)
-                    console.log ( temp )
-                
-            })
-
-            let i = 0;
+            let i = 0
             do {
                 // this is a variable assignment
                 if (content[i].trim().indexOf(" ") == -1){
@@ -73,8 +61,7 @@ module.exports = function(source){
     }
 
     page.data.template_url = `${loader.config.template_dir}/${page.data.template}.pug`
-    this.addDependency( path.resolve( page.data.template_url ) );
+    this.addDependency( path.resolve( page.data.template_url ) )
     page.data.template = fs.readFileSync(page.data.template_url, 'utf8')
-
-    return page;
+    return page
 }
