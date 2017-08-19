9db716ba87267ecce1c27ac9e40cfd8743e68ad6
Cobey Potter <cobey.potter@wellfireinteractive.com>
2017-08-19 00:57:55 -0400

    update how content is stored and retrieved
    when live dev-ing, page.content would be overwritten with the full webpage. resulted in many errors.

--- index.js
+++ index.js
@@ -15,53 +15,57 @@ module.exports = function (source) {
     // lets load the shared locals object
     let locals = loader.locals 
 
-    let page = matter(source)
-
+    const page = matter(source)
+    
+    if (!page.data.content) {
+        page.data.content = page.content
+    }
+        
     // compile content through markdown
     // divide into individual page section key/values if needed
-    if (page.content) {
-
+    if (page.data.content) {
         // trim based on Content Sections `%% key %%` (see README)
-        let content = page.content.trim().split("%%");
+        const content = page.data.content.trim().split("%%");
         
         if (content[0].length == 0) {
             content.shift()
         }
 
         if (content.length == 1) {
-            page.content = content[0]
+            page.data.content = content[0]
         } 
         else {
             let i = 0
             do {
                 // this is a variable assignment
                 if (content[i].trim().indexOf(" ") == -1){
-                    page.data[content[i].trim()] = locals.markdown( content[i + 1] );
-                    i++;
-                    i++;
+                    page.data[content[i].trim()] = locals.markdown( content[i + 1] )
+                    i++
+                    i++
                 } 
                 else {
-                    page["content"] = content[i];
-                    i++;
+                    page.data.content = content[i]
+                    i++
                 }                
-            } while (i < content.length);
+            } while (i < content.length)
         }
 
-        page.content = locals.markdown( page.content );
+        page.data.content = locals.markdown( page.data.content )
+        page.content = ""
     }
 
     // set up the appropriate filename
     if (page.data.slug) {
-        page.resourcePath = `${page.data.slug}`;
+        page.resourcePath = `${page.data.slug}`
     }
 
     // get the associated template
     if (!page.data.template) {
-        page.data.template = loader.config.default_template;
+        page.data.template = loader.config.default_template
     }
 
     page.data.template_url = `${loader.config.template_dir}/${page.data.template}.pug`
     this.addDependency( path.resolve( page.data.template_url ) )
-    page.data.template = fs.readFileSync(page.data.template_url, 'utf8')
+    page.data.template_file = fs.readFileSync(page.data.template_url, 'utf8')
     return page
 }
